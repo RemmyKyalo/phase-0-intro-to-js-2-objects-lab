3ad4f4af4750357516a7630bdf45f5bff63aadce
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2022-11-19 13:40:18 +0300

    commit tree fec78d50b9ba6e84b4cac018fa0ef2b1b9225646

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Dennis',
+    streetAddress: 25
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee };
+    newEmployee[key] = value;
+    key = 'sam';
+    value = '11 Broadway';
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = {...employee };
+    newEmployee[key] = value;
+    delete newEmployee["name"];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
